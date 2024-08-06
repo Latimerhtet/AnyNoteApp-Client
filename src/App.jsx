ee964025d652d99b2a089e93592618eed965476e
Kaung Htet San <latimerhtet@gmail.com>
2024-08-06 19:37:52 +0630

    project starter

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-800 font-bold text-3xl">Hello</h1>
+    </div>
+  );
+};
+
+export default App;
